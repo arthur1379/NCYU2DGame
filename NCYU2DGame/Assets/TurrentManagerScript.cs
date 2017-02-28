86f14d1c5ee4aeaef1db275f3b8f4600a212cd4b
arthur1379 <changhsinlin1206@gmail.com>
2017-02-28 21:47:47 +0800

    UnityWeek02
    我把第二周的進度做完了

---
+++ NCYU2DGame/Assets/TurrentManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TurrentManagerScript : MonoBehaviour {
+
+    private Animator _animator;
+
+    const int DirectionCount = 8;
+    public Ease RotateEaseFunction;
+    public float rotateDuration;
+
+    public Camera GameCamera;
+    public float CameraShakeDuration;
+    public float CameraShakeStrenth;
+
+    // Use this for initialization
+    void Start () {
+        _animator = this.GetComponent<Animator>();
+	}
+
+    private void PlayShootAnimation()
+    {
+        _animator.SetTrigger("Shoot");
+        GameCamera.transform.DOShakePosition(CameraShakeDuration,CameraShakeStrenth);
+    }
+
+    private void PlayRotateAnimation()
+    {
+        float targetDegree = 360.0f / DirectionCount * Random.Range(0, DirectionCount);
+        this.transform.DORotate(new Vector3(0, 0, targetDegree), rotateDuration);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayShootAnimation();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayRotateAnimation();
+        }
+    }
+}
